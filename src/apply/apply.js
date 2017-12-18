f0aba133d9eb27ad3523be4c75043af9dec724b5
omgzui <862275679@qq.com>
2017-12-18 21:25:11 +0800

    集成antd

---
+++ src/apply/apply.js
@@ -0,0 +1,61 @@
+/*
+ * @Author: 小粽子
+ * @Date: 2017-12-18 20:50:11
+ * @Last Modified by: 小粽子
+ * @Last Modified time: 2017-12-18 21:24:29
+ */
+import React, {Component} from 'react';
+import { Layout, Menu, Icon } from 'antd';
+const { Header, Sider, Content } = Layout;
+
+class Apply extends Component {
+  state = {
+    collapsed: false,
+  };
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+  render() {
+    return (
+      <Layout>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span>nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span>nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span>nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+export default Apply;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Apply from './apply/apply';
+import './apply/apply.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Apply/>, document.getElementById('root'));
 registerServiceWorker();
